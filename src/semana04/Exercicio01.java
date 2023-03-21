1bec0d66257781a70e7b256301b52352fd53702b
tharik <tharikmohamadmou@gmail.com>
2023-03-21 20:13:10 -0300

    exercicios da semana04

--- src/semana04/Exercicio01.java
+++ src/semana04/Exercicio01.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio01 {
 
     public static void main(String[] args) {
-        System.out.println("Programa para pegar antecessor e sucessor de um numero");
+        System.out.println("escreva um numero para ver antecessor e sucessor");
         Scanner sc = new Scanner(System.in);
         System.out.println("Entre com o numero: ");
         int numero1 = sc.nextInt();
